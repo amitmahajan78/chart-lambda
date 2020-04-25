cc3868d90fb2056cde0d4533f27733694f2c9ee2
amitmahajan <amitmahajan@post.com>
2020-04-25 16:33:06 +0100

    initial commit

--- src/main/java/org/thesaastech/chart/lambda/AwsS3Service.java
+++ src/main/java/org/thesaastech/chart/lambda/AwsS3Service.java
@@ -1,33 +1,42 @@
 package org.thesaastech.chart.lambda;
 
 
-import com.amazonaws.auth.DefaultAWSCredentialsProviderChain;
-import com.amazonaws.regions.Regions;
-import com.amazonaws.services.s3.AmazonS3;
-import com.amazonaws.services.s3.AmazonS3ClientBuilder;
+import software.amazon.awssdk.auth.credentials.DefaultCredentialsProvider;
+import software.amazon.awssdk.core.sync.RequestBody;
+import software.amazon.awssdk.regions.Region;
+import software.amazon.awssdk.services.s3.S3Client;
+import software.amazon.awssdk.services.s3.model.PutObjectRequest;
 
 import javax.annotation.PostConstruct;
 import javax.enterprise.context.ApplicationScoped;
+import java.io.ByteArrayInputStream;
+import java.io.InputStream;
+import java.nio.charset.StandardCharsets;
 
 @ApplicationScoped
 public class AwsS3Service {
 
     public static final String BUCKET = "s3-stage-dadj872td7g328dw";
 
-    private AmazonS3 s3;
+    private S3Client s3;
 
 
     @PostConstruct
     public void init() {
 
-        s3 = AmazonS3ClientBuilder.standard()
-                .withCredentials(new DefaultAWSCredentialsProviderChain())
-                .withRegion(Regions.EU_WEST_2)
+        s3 = S3Client.builder()
+                .region(Region.EU_WEST_2)
+                .httpClient(software.amazon.awssdk.http.urlconnection.UrlConnectionHttpClient.builder().build())
+                .credentialsProvider(DefaultCredentialsProvider.create())
                 .build();
-
     }
 
     public void createSeObject(String key, String text) {
-        s3.putObject(BUCKET, key, text);
+
+        InputStream inputStream = new ByteArrayInputStream(text.getBytes(StandardCharsets.UTF_8));
+        s3.putObject(PutObjectRequest.builder()
+                .bucket(BUCKET)
+                .key(key)
+                .build(), RequestBody.fromString(text));
     }
 }
